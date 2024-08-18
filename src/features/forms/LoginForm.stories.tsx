3a653a896dbd3c9c5f19961fe7a0cc1342b69593
Sinet <yunisovns@gmail.com>
2024-08-18 15:56:53 +0300

    ДЗ 10 от 18.08 v2

---
+++ src/features/forms/LoginForm.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import LoginForm from './LoginForm';
+
+const meta: Meta<typeof LoginForm> = {
+  title: 'shared/LoginForm',
+  component: LoginForm,
+  args: {},
+};
+
+export default meta;
+
+export const Default = {};
---
+++ src/features/forms/RegForm.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import RegForm from './RegForm';
+
+const meta: Meta<typeof RegForm> = {
+  title: 'shared/RegForm',
+  component: RegForm,
+  args: {},
+};
+
+export default meta;
+
+export const Default = {};
